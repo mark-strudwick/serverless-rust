3d1758aa80e6f0b910a1e288f899069e57bb52d4
Mark <markstrudwick@outlook.com>
2023-02-15 21:43:28 +0000

    create apigw

--- infrastructure/lib/cdk-stack.ts
+++ infrastructure/lib/cdk-stack.ts
@@ -2,17 +2,36 @@ import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { RustFunction } from 'cargo-lambda-cdk';
+import {HttpApi, HttpMethod} from '@aws-cdk/aws-apigatewayv2-alpha';
+import {HttpLambdaIntegration} from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const func = new RustFunction(this, 'rust-function', {
-      manifestPath: path.join(__dirname, '..', '..')
+      manifestPath: path.join(__dirname, '..', '..'),
+      functionName: 'rust-function',
+    });
+
+    const apigw = new HttpApi(this, 'apigw', {
+      apiName: 'serverless-rust',
+    });
+
+    apigw.addRoutes({
+      path: '/',
+      methods: [HttpMethod.ANY],
+      integration: new HttpLambdaIntegration(
+        'function-integration',
+        func
+      ), 
     });
 
     new cdk.CfnOutput(this, 'function name', {
       value: func.functionName,
+    });
+    new cdk.CfnOutput(this, 'api endpoint', {
+      value: apigw.apiEndpoint,
     });
   }
 }
