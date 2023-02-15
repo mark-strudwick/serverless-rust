6d0778dc54a6e6170eb92c52d4eb95540d4dcecc
Mark <markstrudwick@outlook.com>
2023-02-15 18:33:50 +0000

    Create lambda function

---
+++ backend/src/main.rs
@@ -0,0 +1,32 @@
+use lambda_http::{
+    run,
+    Error,
+};
+use axum::{
+    response::Json,
+    Router,
+    routing::get,
+};
+use serde_json::{Value, json};
+
+async fn root() -> Json<Value> {
+    Json(json!({
+        "message": "Hello, World!",
+    }))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    let app = Router::new()
+        .route("/",  get(root));
+
+    run(app).await
+}
